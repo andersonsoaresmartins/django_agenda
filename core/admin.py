f8b99d30c77ea0f7bbd137c0e7a314fa605a743a
andersonsoaresmartins <asmartins@live.com>
2023-10-09 08:10:06 -0300

    Add delete and edit features

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from core.models import Evento
 
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'data_criacao', 'usuario')
+    list_display = ('id', 'titulo', 'data_evento', 'data_criacao', 'usuario')
 
 
 # Register your models here.
